3a655d8855ee5d042ed016c0ee15a02ca98ce193
MarcoQin <qyyfy2009@gmail.com>
2019-04-09 20:15:09 +0800

    update lava-core

--- build.rs
+++ build.rs
@@ -1,8 +1,13 @@
 extern crate cc;
+use std::process::Command;
 
 fn main() {
+    Command::new("git")
+        .args(&["submodule", "update", "--init"])
+        .output()
+        .expect("failed to update submodule");
     cc::Build::new()
-        .file("src/player.c")
+        .file("src/lava-core/core.c")
         .static_flag(true)
         .compile("libplayer.a");
 }
--- src/player.rs
+++ src/player.rs
@@ -2,71 +2,58 @@ extern crate libc;
 use std::ffi::CString;
 
 extern {
-    fn global_init();
-    fn cp_load_file(file_name: *const libc::c_char);
-    fn cp_pause_audio();
-    fn cp_stop_audio();
-    fn cp_set_volume(volume: libc::c_int);
-    fn cp_seek_audio(percent: libc::c_double);
-    fn cp_seek_audio_by_sec(sec: libc::c_int);
-    fn cp_seek_audio_by_absolute_pos(pos: libc::c_int);
-    fn cp_get_time_length() -> libc::c_int;
-    fn cp_is_stopping() -> libc::c_int;
-    fn cp_get_current_time_pos() -> libc::c_double;
+    fn load_file(file_name: *const libc::c_char);
+    fn pause_audio();
+    fn stop_audio();
+    fn set_volume(volume: libc::c_int);
+    fn seek_audio(percent: libc::c_double);
+    fn seek_audio_by_sec(sec: libc::c_int);
+    fn get_time_length() -> libc::c_int;
+    fn is_stopping() -> libc::c_int;
+    fn get_current_time_pos() -> libc::c_double;
 }
 
-pub fn init_player() {
-    unsafe {
-        global_init();
-    }
-}
-
-pub fn load_file(filename: String) {
+pub fn play(filename: String) {
     unsafe {
         let _path = CString::new(filename).unwrap();
-        cp_load_file(_path.as_ptr());
+        load_file(_path.as_ptr());
     }
 }
 
 pub fn pause() {
     unsafe {
-        cp_pause_audio();
+        pause_audio();
     }
 }
 
 pub fn stop() {
     unsafe {
-        cp_stop_audio();
+        stop_audio();
     }
 }
 
-pub fn set_volume(volume: i32) {
+pub fn set_volume_to(volume: i32) {
     unsafe {
-        cp_set_volume(volume);
+        set_volume(volume);
     }
 }
 
 pub fn seek_by_percent(percent: f64) {
     unsafe {
-        cp_seek_audio(percent);
+        seek_audio(percent);
     }
 }
 
 pub fn seek_by_second(sec: i32) {
     unsafe {
-        cp_seek_audio_by_sec(sec);
+        seek_audio_by_sec(sec);
     }
 }
 
-pub fn seek_by_position(pos: i32) {
-    unsafe {
-        cp_seek_audio_by_absolute_pos(pos);
-    }
-}
 
 pub fn get_current_time_length() -> i32 {
     unsafe {
-        let length = cp_get_time_length();
+        let length = get_time_length();
         length
     }
 }
@@ -74,14 +61,14 @@ pub fn get_current_time_length() -> i32 {
 pub fn get_current_time_position() -> f64 {
 
     unsafe {
-        let pos = cp_get_current_time_pos();
+        let pos = get_current_time_pos();
         pos
     }
 }
 
-pub fn is_stopping() -> bool {
+pub fn stopping() -> bool {
     unsafe {
-        let is_stopping = cp_is_stopping();
+        let is_stopping = is_stopping();
         if is_stopping > 0 {
             return true;
         } else {
